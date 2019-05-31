19c80ec90e7128ac113f3d830f4c7d7db54220b4
DimaGrish <admin@gmail.com>
2019-05-31 23:39:53 +0300

    lesson 5,6

--- class.py
+++ class.py
@@ -1,6 +1,23 @@
 _author_ = 'Dmitry'
 
 from datetime import date
+from geomP import *
+
+
+a = Point(5, 5)
+b = Point(10, 15)
+
+print(a == b)
+print(b == Point(10, 15))
+print(a.distance(b))
+
+
+
+
+
+
+
+
 
 
 
---
+++ geomP/__init__.py
@@ -0,0 +1,1 @@
+from geomP.point import *
