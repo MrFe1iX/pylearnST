7be7a27ba8e1e72d5d1f954982bbb59f6f0032f0
DimaGrish <admin@gmail.com>
2019-05-31 22:52:53 +0300

    lesson 4

---
+++ import.py
@@ -0,0 +1,16 @@
+_author_ = 'Dmitry'
+
+import math
+from math import sin
+#from math import *
+from math import cos as c
+
+one = math.sqrt(9)
+print(one)
+
+two = sin(5)
+print(two)
+
+three = c(0)
+print(three)
+
