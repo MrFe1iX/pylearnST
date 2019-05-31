611111091c38b1f25f38f4f7a4138366944e4659
DimaGrish <admin@gmail.com>
2019-05-31 23:13:30 +0300

    lesson 5

---
+++ class.py
@@ -0,0 +1,16 @@
+_author_ = 'Dmitry'
+
+from datetime import date
+
+
+
+data_in_past = date(2001, 5, 15)
+
+
+print(data_in_past.year)
+print(data_in_past.month)
+print(data_in_past.day)
+print(data_in_past.weekday())
+
+
+print(type(data_in_past))
