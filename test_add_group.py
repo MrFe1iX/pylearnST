16fb41cde2298d5974a60ca4d7e1e69dfc622a4b
Dmitry Grishin <47281779+mrfe1ix@users.noreply.github.com>
2019-06-01 01:51:01 +0300

    refactor code

--- test_add_group.py
+++ test_add_group.py
@@ -11,14 +11,20 @@ class TestAddGroup(unittest.TestCase):
 
     def test_add_group(self):
         wd = self.wd
-        # Открыта страница
-        wd.get("http://localhost/addressbook/")
-        # Логин
-        wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys("admin")
-        wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys("secret")
-        wd.find_element_by_xpath("//input[@value='Login']").click()
+        self.open_home_page(wd)
+        self.login(wd)
+        self.create_group(wd)
+        self.return_to_groups_page(wd)
+        self.logout(wd)
+
+    def logout(self, wd):
+        # Выйти
+        wd.find_element_by_link_text("Logout").click()
+
+    def return_to_groups_page(self, wd):
+        wd.find_element_by_link_text("group page").click()
+
+    def create_group(self, wd):
         # Открыть группы
         wd.find_element_by_link_text("groups").click()
         # Нажать на новая группа и заполнение
@@ -32,9 +38,18 @@ class TestAddGroup(unittest.TestCase):
         wd.find_element_by_name("group_footer").send_keys("SUPER CLASS")
         # Сохранить
         wd.find_element_by_name("submit").click()
-        wd.find_element_by_link_text("group page").click()
-        # Выйти
-        wd.find_element_by_link_text("Logout").click()
+
+    def login(self, wd):
+        # Логин
+        wd.find_element_by_name("user").clear()
+        wd.find_element_by_name("user").send_keys("admin")
+        wd.find_element_by_name("pass").clear()
+        wd.find_element_by_name("pass").send_keys("secret")
+        wd.find_element_by_xpath("//input[@value='Login']").click()
+
+    def open_home_page(self, wd):
+        # Открыта дом страница
+        wd.get("http://localhost/addressbook/")
 
     def tearDown(self):
         self.wd.quit()
