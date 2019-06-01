1b77ae45b3b0443b34b4376c2b23041575a05da9
DimaGrish <admin@gmail.com>
2019-06-01 22:38:22 +0300

    add class User

--- obj_class.py
+++ obj_class.py
@@ -9,5 +9,14 @@ class Group:
 
 class User:
 
-    def __init__(self):
-        pass
+    def __init__(self, firstname, middlename, lastname, nick,
+                 email, address, month, day, year):
+        self.firstname = firstname
+        self.middlename = middlename
+        self.lastname = lastname
+        self.nick = nick
+        self.email = email
+        self.address = address
+        self.month = month
+        self.day = day
+        self.year = year
--- test_add_user.py
+++ test_add_user.py
@@ -13,15 +13,14 @@ class TestAddUser(unittest.TestCase):
         wd = self.wd
         self.open_home_page(wd)
         self.login(wd, username="admin", password="secret")
-        self.add_user_info(wd, firstname="Maxim", middlename="M", lastname="Semenov", nick="max", email="test@test.ru",
-                           address="Moskovskaz 12", month=5, day="6", year="1984")
+        self.add_user_info(wd, User(firstname="Maxim", middlename="M", lastname="Semenov", nick="max", email="test@test.ru",
+                           address="Moskovskaz 12", month=5, day="6", year="1984"))
         self.logout(wd)
 
     def logout(self, wd):
         wd.find_element_by_link_text("Logout").click()
 
-    def add_user_info(self, wd, firstname, middlename, lastname,
-                      nick, email, year, address, month, day):
+    def add_user_info(self, wd, user):
         month_book = {1: "January", 2: "February", 3: "March", 4: "April", 5: "May", 6: "June",
                       7: "July", 8: "August", 9: "September", 10: "October", 11: "November", 12: "December"
                       }
@@ -29,22 +28,22 @@ class TestAddUser(unittest.TestCase):
         wd.find_element_by_link_text("add new").click()
         # Fill
         wd.find_element_by_name("firstname").clear()
-        wd.find_element_by_name("firstname").send_keys(firstname)
+        wd.find_element_by_name("firstname").send_keys(user.firstname)
         wd.find_element_by_name("middlename").clear()
-        wd.find_element_by_name("middlename").send_keys(middlename)
+        wd.find_element_by_name("middlename").send_keys(user.middlename)
         wd.find_element_by_name("lastname").clear()
-        wd.find_element_by_name("lastname").send_keys(lastname)
+        wd.find_element_by_name("lastname").send_keys(user.lastname)
         wd.find_element_by_name("nickname").clear()
-        wd.find_element_by_name("nickname").send_keys(nick)
+        wd.find_element_by_name("nickname").send_keys(user.nick)
         wd.find_element_by_name("email").clear()
-        wd.find_element_by_name("email").send_keys(email)
-        wd.find_element_by_css_selector("option[value=" + day + "]").click()
+        wd.find_element_by_name("email").send_keys(user.email)
+        wd.find_element_by_css_selector("option[value=" + "'" + str(user.day) + "'" + "]").click()
         wd.find_element_by_name("bmonth").click()
-        wd.find_element_by_css_selector("option[value=" + month_book[month] + "]").click()
+        wd.find_element_by_css_selector("option[value=" + month_book[user.month] + "]").click()
         wd.find_element_by_name("byear").clear()
-        wd.find_element_by_name("byear").send_keys(year)
+        wd.find_element_by_name("byear").send_keys(user.year)
         wd.find_element_by_name("address").clear()
-        wd.find_element_by_name("address").send_keys(address)
+        wd.find_element_by_name("address").send_keys(user.address)
         # Save
         wd.find_element_by_xpath("(//input[@name='submit'])[2]").click()
 
