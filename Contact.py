dbc705c8e2b70c46e73d4f70a3ab50de8f6cd879
Dmitry Grishin <47281779+MrFe1iX@users.noreply.github.com>
2019-06-03 15:49:22 +0300

    Create contact

---
+++ Contact.py
@@ -0,0 +1,13 @@
+class Contact:
+
+    def __init__(self, firstname, middlename, lastname, nick,
+                 email, address, month, day, year):
+        self.firstname = firstname
+        self.middlename = middlename
+        self.lastname = lastname
+        self.nick = nick
+        self.email = email
+        self.address = address
+        self.month = month
+        self.day = day
+        self.year = year
