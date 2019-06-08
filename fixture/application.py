03eb0e3cc7b25e061945566a88b7c1e5fda11e33
Dmitry Grishin <felix63rus@gmail.com>
2019-06-08 20:54:58 +0300

    Декомпазиция теста с котактами

--- fixture/application.py
+++ fixture/application.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from fixture.session import SessionHelper
 from fixture.group import GroupHelper
+from fixture.contact import ContactHelper
 
 
 class Application:
@@ -10,38 +11,11 @@ class Application:
         self.wd.implicitly_wait(30)
         self.session = SessionHelper(self)
         self.group = GroupHelper(self)
+        self.contact = ContactHelper(self)
 
     def open_home_page(self):
         wd = self.wd
         wd.get("http://localhost/addressbook/")
-
-    def add_user_info(self, contact):
-        wd = self.wd
-        month_book = {1: "January", 2: "February", 3: "March", 4: "April", 5: "May", 6: "June",
-                      7: "July", 8: "August", 9: "September", 10: "October", 11: "November", 12: "December"
-                      }
-        # Click on add user
-        wd.find_element_by_link_text("add new").click()
-        # Fill
-        wd.find_element_by_name("firstname").clear()
-        wd.find_element_by_name("firstname").send_keys(contact.firstname)
-        wd.find_element_by_name("middlename").clear()
-        wd.find_element_by_name("middlename").send_keys(contact.middlename)
-        wd.find_element_by_name("lastname").clear()
-        wd.find_element_by_name("lastname").send_keys(contact.lastname)
-        wd.find_element_by_name("nickname").clear()
-        wd.find_element_by_name("nickname").send_keys(contact.nick)
-        wd.find_element_by_name("email").clear()
-        wd.find_element_by_name("email").send_keys(contact.email)
-        wd.find_element_by_css_selector("option[value=" + "'" + str(contact.day) + "'" + "]").click()
-        wd.find_element_by_name("bmonth").click()
-        wd.find_element_by_css_selector("option[value=" + month_book[contact.month] + "]").click()
-        wd.find_element_by_name("byear").clear()
-        wd.find_element_by_name("byear").send_keys(contact.year)
-        wd.find_element_by_name("address").clear()
-        wd.find_element_by_name("address").send_keys(contact.address)
-        # Save
-        wd.find_element_by_xpath("(//input[@name='submit'])[2]").click()
 
     def death(self):
         self.wd.quit()
--- test/test_add_user.py
+++ test/test_add_user.py
@@ -12,8 +12,8 @@ def app(request):
 
 
 def test_add_user(app):
-    app.login(username="admin", password="secret")
-    app.add_user_info(Contact(firstname="Test", middlename="OlO",
-                               lastname="Testovich", nick="tes", email="test@test.ru",
-                               address="Moskovskaz 12", month=2, day="10", year="1984"))
-    app.logout()
+    app.session.login(username="admin", password="secret")
+    app.contact.add_user_info(Contact(firstname="Test", middlename="OlO",
+                                lastname="Testovich", nick="tes", email="test@test.ru",
+                                address="Moskovskaz 12", month=2, day="10", year="1984"))
+    app.session.logout()
