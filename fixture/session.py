40866bb78e0805081d8b82c561b953e55687b483
Dmitry Grishin <felix63rus@gmail.com>
2019-06-09 14:57:52 +0300

    add conftest.py

--- fixture/session.py
+++ fixture/session.py
@@ -5,6 +5,11 @@ class SessionHelper:
     def __init__(self, app):
         self.app = app
 
+    def logout(self):
+        wd = self.app.wd
+        # wd.find_element_by_link_text("Logout").click()
+        wd.find_element_by_xpath("//*[@id='top']/form/a").click()
+
     def login(self, username, password):
         wd = self.app.wd
         self.app.open_home_page()
@@ -14,6 +19,10 @@ class SessionHelper:
         wd.find_element_by_name("pass").send_keys(password)
         wd.find_element_by_xpath("//input[@value='Login']").click()
 
-    def logout(self):
-        wd = self.app.wd
-        wd.find_element_by_link_text("Logout").click()
+
+
+
+
+
+
+
