3714b0ff1fcab35db6d871d6208f954bc42f0e14
Dmitry Grishin <felix63rus@gmail.com>
2019-06-11 23:12:29 +0300

    fix del user

--- fixture/contact.py
+++ fixture/contact.py
@@ -50,6 +50,8 @@ class ContactHelper:
         wd = self.app.wd
         wd.find_element_by_name("selected[]").click()
         wd.find_element_by_xpath("//input[@value='Delete']").click()
+        wd.switch_to_alert().accept()
+        self.app.open_home_page
 
     def del_all_user(self):
         wd = self.app.wd
