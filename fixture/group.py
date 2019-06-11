32ad76f7cb447a137fbfb1b2d43f6b8f34f13020
Dmitry Grishin <felix63rus@gmail.com>
2019-06-11 23:11:52 +0300

    add fill method

--- fixture/group.py
+++ fixture/group.py
@@ -5,18 +5,21 @@ class GroupHelper:
     def __init__(self, app):
         self.app = app
 
-    def create(self, group):
+    def fill_group_form(self, group):
         wd = self.app.wd
-        # Нажать на новая группа и заполнение
-        self.open_group_page()
-        wd.find_element_by_name("new").click()
-        wd.find_element_by_name("group_name").click()
         wd.find_element_by_name("group_name").clear()
         wd.find_element_by_name("group_name").send_keys(group.name)
         wd.find_element_by_name("group_header").clear()
         wd.find_element_by_name("group_header").send_keys(group.header)
         wd.find_element_by_name("group_footer").clear()
         wd.find_element_by_name("group_footer").send_keys(group.footer)
+
+    def create(self, group):
+        wd = self.app.wd
+        # Нажать на новая группа и заполнение
+        self.open_group_page()
+        wd.find_element_by_name("new").click()
+        self.fill_group_form(group)
         # Сохранить
         wd.find_element_by_name("submit").click()
         self.return_to_group_page()
@@ -26,12 +29,7 @@ class GroupHelper:
         self.open_group_page()
         wd.find_element_by_name("selected[]").click()
         wd.find_element_by_name("edit").click()
-        wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys(group.name)
-        wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys(group.header)
-        wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys(group.footer)
+        self.fill_group_form(group)
         # Сохранить
         wd.find_element_by_name("update").click()
         self.return_to_group_page()
