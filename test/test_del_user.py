d3257fdb44c0b854ca8d0a04ee63ce79b5de55d7
Dmitry Grishin <felix63rus@gmail.com>
2019-06-14 20:38:04 +0300

    code style

--- test/test_del_user.py
+++ test/test_del_user.py
@@ -6,8 +6,6 @@ def test_del_first_user(app):
     app.contact.del_edit_user()
     app.session.logout()
 
-
-
 # def test_del_all_user(app):
 #     app.session.login(username="admin", password="secret")
 #     app.contact.del_all_user()
