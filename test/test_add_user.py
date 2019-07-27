d4c46d48843d2410f4ac27e2db6c7f5752cf706e
MrFe1iX <FeliX63rus@gmail.com>
2019-07-27 16:14:20 +0300

    Фик добавил проверку из бд в тесте добавления юзера

--- test/test_add_user.py
+++ test/test_add_user.py
@@ -2,12 +2,12 @@
 from model.contact import Contact
 
 
-def test_add_user(app, data_users):
+def test_add_user(app, db, data_users):
     user = data_users
-    old_user_list = app.contact.get_user_list()
+    old_user_list = db.get_user_list()
     app.contact.add_new_user(user)
     assert len(old_user_list) + 1 == app.contact.count()
-    new_user_list = app.contact.get_user_list()
+    new_user_list = db.get_user_list()
     old_user_list.append(user)
     assert sorted(old_user_list, key=Contact.id_or_max) == sorted(new_user_list, key=Contact.id_or_max)
 
